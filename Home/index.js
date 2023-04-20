cc71439a3ffcd14d799350483c4781a5ac8725ff
kuntalkumar <kuntalkumar789@gmail.com>
2023-04-20 23:33:41 +0530

    dashboard

---
+++ Home/index.js
@@ -0,0 +1,32 @@
+
+// var medicine=document.getElementById("medicine").value;
+// var labTest=document.getElementById("labTest").value;
+// var healthcare=document.getElementById("healthcare").value;
+// var healthBlog=document.getElementById("healthBlog").value;
+// var plus=document.getElementById("plus").value;
+// var offer=document.getElementById("offer").value;
+// var valueStore=document.getElementById("valueStore").value;
+// var arr=[]
+// var obj={
+    
+//     medicine:medicine,
+//     labTest:labTest,
+//     healthcare:healthcare,
+//     healthBlog:healthBlog,
+//     plus:plus,
+//     offer:offer,
+//     valueStore:valueStore
+
+// }
+// arr.push(obj);
+// console.log(arr);
+
+
+ document.getElementById("deliver").addEventListener("click",fun);
+
+
+function fun(){
+
+  var addresses= document.getElementById("EnterAddress").textContent= prompt("Please Enter your address");
+  addresses
+}
