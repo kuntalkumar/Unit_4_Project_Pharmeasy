e1ee08c65438be14b5bb6c703c9a6344b328cbe0
kuntalkumar <kuntalkumar789@gmail.com>
2023-04-23 02:37:55 +0530

    product details page creater successfuly

--- Home/index.js
+++ Home/index.js
@@ -1,26 +1,4 @@
 
-// var medicine=document.getElementById("medicine").value;
-// var labTest=document.getElementById("labTest").value;
-// var healthcare=document.getElementById("healthcare").value;
-// var healthBlog=document.getElementById("healthBlog").value;
-// var plus=document.getElementById("plus").value;
-// var offer=document.getElementById("offer").value;
-// var valueStore=document.getElementById("valueStore").value;
-// var arr=[]
-// var obj={
-    
-//     medicine:medicine,
-//     labTest:labTest,
-//     healthcare:healthcare,
-//     healthBlog:healthBlog,
-//     plus:plus,
-//     offer:offer,
-//     valueStore:valueStore
-
-// }
-// arr.push(obj);
-// console.log(arr);
-
 
 document.getElementById("deliver").addEventListener("click",fun);
 
